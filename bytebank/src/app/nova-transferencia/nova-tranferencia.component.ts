01fcde45e266c3d2ac99a35004402b4645c3fe57
GustavoMeloGit <gustavo.melo_10@hotmail.com>
2021-08-22 15:44:24 -0300

    continuacao dos estudo de angular

--- bytebank/src/app/nova-transferencia/nova-tranferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-tranferencia.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-nova-tranferencia',
-    templateUrl:'./nova-tranferencia.component.html',
-    styleUrls:['./nova-tranferencia.component.scss']
+  selector: 'app-nova-tranferencia',
+  templateUrl: './nova-tranferencia.component.html',
+  styleUrls: ['./nova-tranferencia.component.scss'],
 })
-export class NovaTranferenciaComponent{
+export class NovaTranferenciaComponent {
+  valor :number = 0;
+  destino: number = 0;
 
+  transferir() {
+    console.log('Solicitada nova tranferência');
+    console.log('Valor: ', this.valor);
+    console.log('Destino', this.destino);
+  }
 }
